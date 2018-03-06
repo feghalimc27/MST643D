21aa23273985d1ff74aa9f051afe2288d691ccfd
mibikin <mibikin@gmail.com>
2018-03-05 17:22:42 -0700

    Added basic tilt
    Controller can tilt left and right, still need to get forward and back tilting. Basic physics are also included

--- Assets/Scripts/GroundTiltControl.cs
+++ Assets/Scripts/GroundTiltControl.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class GroundTiltControl : MonoBehaviour {
+
+    public float tiltAngle;
+
+    private Vector2 planeTilt;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +16,16 @@ public class GroundTiltControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        TiltControl();
 	}
+
+    void TiltControl() {
+        Vector2 tilt = new Vector2(Input.GetAxis("Horizontal") * tiltAngle, Input.GetAxis("Vertical") * tiltAngle);
+
+        planeTilt.y = Mathf.Clamp(tilt.y, -tiltAngle, tiltAngle);
+        planeTilt.x = Mathf.Clamp(tilt.x, -tiltAngle, tiltAngle);
+
+        transform.localRotation = Quaternion.AngleAxis(tilt.y, Vector3.right);
+        transform.localRotation = Quaternion.AngleAxis(tilt.x, Vector3.back);
+    }
 }
