176744158c1d590a4b9bc7024033f93e0ee6925d
mibikin <mibikin@gmail.com>
2018-03-05 17:00:24 -0700

    Initial commit
    Base project with a few scripts and a test world for the first super monkey ball level

---
+++ Assets/Scripts/FPSMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FPSMovement: MonoBehaviour {
+
+    public float speed = 10.0f;
+    public float jumpStrength = 25.0f;
+
+    private bool onGround = false;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    // Called at a consistent point per frame
+    void FixedUpdate() {
+        MovePlayer();
+        Jump();
+    }
+
+    void OnCollisionStay(Collision col) {
+        // Layer 8 = Ground
+        if (col.gameObject.layer == 8) {
+            onGround = true;
+        }
+    }
+
+    void OnCollisionExit(Collision col) {
+        // Layer 8 = Ground
+        if (col.gameObject.layer == 8) {
+            onGround = false;
+        }
+    }
+
+    // Move player
+    void MovePlayer() {
+        // Movement strengths
+        float forwardPos = Input.GetAxis("Vertical") * speed;
+        float strafePos = Input.GetAxis("Horizontal") * speed;
+
+        // Normalize
+        strafePos *= Time.deltaTime;
+        forwardPos *= Time.deltaTime;
+
+        // Translate by strength
+        transform.Translate(strafePos, 0, forwardPos);
+
+        // Allow cursor to be freed on Esc press
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    void Jump() {
+        if (onGround && Input.GetButtonDown("Jump")) {
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, jumpStrength, 0));
+        }
+    }
+}
---
+++ Assets/Scripts/FirstPersonCameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCameraController : MonoBehaviour {
+
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
+
+    private Vector2 mouseLook;
+    private Vector2 smoothV;
+
+    private GameObject character;
+
+	// Use this for initialization
+	void Start () {
+        character = this.transform.parent.gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        MouseLook();
+    }
+
+    void MouseLook() {
+        Vector2 md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+        mouseLook += smoothV;
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -87.0f, 87.0f);
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+    }
+}
