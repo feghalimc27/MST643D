d8846860880b479704a59223f89366e6256eccfa
mibikin <mibikin@gmail.com>
2018-03-08 14:17:12 -0700

    Updated camera; added ball force
    Updated the camera to now follow the ball and sit closer to the ball. Ball movement now applys a force to the ball. Issues with plane tilt can cause the ball to clip into the plane and fall into the abbyss. Still working out how to solve this, may only require a thicker plane but also might need updated tilting code. Tested with GameCube Controller and works correctly

---
+++ Assets/Scripts/BallCameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCameraFollow : MonoBehaviour {
+
+    public float distance;
+    public GameObject character;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        UpdatePosition();
+    }
+
+    void UpdatePosition() {
+        float x = character.transform.position.x;
+        float y = character.transform.position.y + distance / 2;
+        float z = character.transform.position.z - distance;
+
+        transform.position = new Vector3(x, y, z);
+    }
+}
---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallMovement : MonoBehaviour {
+
+    public float acceleration;
+    public float friction;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void FixedUpdate() {
+        AccelerateBall();
+    }
+
+    void AccelerateBall() {
+        GetComponent<Rigidbody>().AddForce(acceleration * Input.GetAxis("Horizontal"), 0, acceleration * Input.GetAxis("Vertical"));
+    }
+
+    void ApplyFriction() {
+
+    }
+}
