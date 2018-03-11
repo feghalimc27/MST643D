989157e5c7a9b6db6f9a0270b4c0210c3601a60a
Uriah Moeller <uriah.moeller@ucdenver.edu>
2018-03-10 19:46:43 -0700

    Revert "Merge branch 'master' of https://github.com/mibikin/Merry-Seoul-Thoughts-64-3D"
    This reverts commit 7cf833157d362e41078fa9e78868a6a76190a5d1, reversing
    changes made to 99ef469bb1c09923031281a15955f715c45bb5d2.

--- Assets/Scripts/CameraControllerFollower.cs
+++ Assets/Scripts/CameraControllerFollower.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,23 +16,3 @@ public class CameraControllerFollower : MonoBehaviour {
 		transform.position = character.transform.position;
 	}
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CameraControllerFollower : MonoBehaviour {
-
-	public GameObject character;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		transform.position = character.transform.position;
-	}
-}
->>>>>>> 2a67ffc42b555822b778f80321b869dfa415ab88
