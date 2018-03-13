e0d5350cef567e1996593576c3b8de7969b951db
mibikin <mibikin@gmail.com>
2018-03-12 21:09:18 -0600

    Added ground and controllers to FPS level. Can be played wither with KB+M or Controller

--- Assets/All Scenes/3. Density/Scripts/FirstPersonCameraController.cs
+++ Assets/All Scenes/3. Density/Scripts/FirstPersonCameraController.cs
@@ -7,6 +7,8 @@ public class FirstPersonCameraController : MonoBehaviour {
     public float sensitivity = 5.0f;
     public float smoothing = 2.0f;
 
+    public bool controller = false;
+
     private Vector2 mouseLook;
     private Vector2 smoothV;
 
@@ -19,7 +21,12 @@ public class FirstPersonCameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        MouseLook();
+        if (!controller) {
+            MouseLook();
+        }
+        else {
+            ControllerLook();
+        }
     }
 
     void MouseLook() {
@@ -32,6 +39,19 @@ public class FirstPersonCameraController : MonoBehaviour {
         mouseLook.y = Mathf.Clamp(mouseLook.y, -87.0f, 87.0f);
 
         transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+    }
+
+    void ControllerLook() {
+        Vector2 md = new Vector2(Input.GetAxisRaw("HorizontalR"), Input.GetAxisRaw("VerticalR"));
+
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+        mouseLook += smoothV;
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -87.0f, 87.0f);
+
+        transform.localRotation = Quaternion.AngleAxis(mouseLook.y, Vector3.right);
         character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
     }
 }
