b17160c6f768352a16268da32dc3334932071f21
mibikin <mibikin@gmail.com>
2018-03-14 11:41:38 -0600

    Added platform rotate script

---
+++ Assets/All Scenes/2. Super Seoul Ball 3D/Scripts/RotatePlatform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlatform : MonoBehaviour {
+
+	public float ySpeed;
+	public float xSpeed;
+	public float zSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(xSpeed * Time.deltaTime, ySpeed * Time.deltaTime, zSpeed * Time.deltaTime);
+	}
+}
