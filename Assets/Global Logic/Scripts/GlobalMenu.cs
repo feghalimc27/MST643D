83e0952ca70dd4f0011cbb92cff7647c21e08eeb
Uriah Moeller <uriah.moeller@ucdenver.edu>
2018-03-26 12:15:08 -0600

    Pause Button Code

--- Assets/Global Logic/Scripts/GlobalMenu.cs
+++ Assets/Global Logic/Scripts/GlobalMenu.cs
@@ -1,10 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GlobalMenu : MonoBehaviour
 {
-	void OnEnable ()
+    public Button retryButton;
+    public Button quitToMenuButton;
+    public Button quitToDesktopButton;
+
+    void Start()
+    {
+        Button retryButtonPress = retryButton.GetComponent<Button>();
+        Button quitToMenuButtonPress = quitToMenuButton.GetComponent<Button>();
+        Button quitToDesktopButtonPress = quitToDesktopButton.GetComponent<Button>();
+        retryButtonPress.onClick.AddListener(() => { SceneManager.LoadScene(SceneManager.GetActiveScene().name); });
+        quitToMenuButtonPress.onClick.AddListener(() => { SceneManager.LoadScene(0); });
+        quitToDesktopButtonPress.onClick.AddListener(() => { Application.Quit(); });
+    }
+
+    void OnEnable ()
     {
         Time.timeScale = 0;
 	}
