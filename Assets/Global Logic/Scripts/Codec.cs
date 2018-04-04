b8d1878f6f064192aa25df2613a2ca1000456ab1
Uriah Moeller <uriah.moeller@ucdenver.edu>
2018-04-04 13:04:20 -0600

    Added Codec Script

---
+++ Assets/Global Logic/Scripts/Codec.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Codec : MonoBehaviour
+{
+    public RawImage bar1;
+    public RawImage bar2;
+    public RawImage bar3;
+    public RawImage bar4;
+    public RawImage bar5;
+    public RawImage bar6;
+    public RawImage bar7;
+    public RawImage bar8;
+    public RawImage bar9;
+
+    RawImage[] allBars;
+
+    void Start ()
+    {
+        allBars = new RawImage[] { bar1, bar2, bar3, bar4, bar5, bar6, bar7, bar8, bar9 };
+
+    }
+
+	void Update ()
+    {
+		
+	}
+}
