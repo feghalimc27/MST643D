8ad29033b0407471542f18c992bace3fa78d7473
Uriah Moeller <uriah.moeller@ucdenver.edu>
2018-04-07 15:51:35 -0600

    Codec Updated

--- Assets/Global Logic/Scripts/Codec.cs
+++ Assets/Global Logic/Scripts/Codec.cs
@@ -16,26 +16,79 @@ public class Codec : MonoBehaviour
     public Texture bar9;
 
     Texture[] allBars;
+    string[] allLines;
+    int currentString;
+    int currentPos;
 
     void Start ()
     {
         allBars = new Texture[] { bar1, bar2, bar3, bar4, bar5, bar6, bar7, bar8, bar9 };
+        allLines = new string[] { "I used to be able to keep going with these lines, you know.\nThough whoever coded them got lazy I guess.",
+                                  "I can't let you do that, Starmerry.",
+                                  "Mo has been fired " + Random.Range(500, 50000) + " times, apparently.",
+                                  "Ha ha! This next game will be your end!\nI have no idea what game it is, though, since whoever wrote these lines got too lazy.",
+                                  "Who named this game?\nWhat does Merry Seoul Thoughts even stand for?",
+                                  "I fight for my friends.",
+                                  "Sure is taking you a while to beat this game, huh?\nI'm not even sure you'll finish, at this rate.",
+                                  "Who named these games anyways? It's pretty obvious everything is just ripped off.\nLike, Alley Combatant? Obviously just Stree-",
+                                  "Ban wobbling.",
+                                  "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+                                  "Merry, turn the game console off right now!\nThe mission is a failure! Cut the power right now!\n. . .\nWait, what?",
+                                  "Are you alright?\nSnake?\nSnake!?\nSNAAAAAKE!",
+                                  "Hike Maze died for this.",
+                                  "The FitnessGram Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start." };
+        currentString = Random.Range(0, allLines.Length);
+        textType();
     }
 
     void OnEnable()
     {
+        Time.timeScale = 0;
         Invoke("mikeTalking", 0f);
+        transform.Find("Text Line").GetComponent<Text>().text = "";
+        transform.Find("Text Line 2").GetComponent<Text>().text = "";
+        currentPos = 0;
     }
 
     void OnDisable()
     {
+        Time.timeScale = 1;
         CancelInvoke("mikeTalking");
+        transform.Find("Text Line").GetComponent<Text>().text = "";
+        transform.Find("Text Line 2").GetComponent<Text>().text = "";
+        currentPos = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Submit"))
+        {
+            transform.gameObject.SetActive(false);
+        }
     }
 
     void mikeTalking()
     {
+        transform.Find("Bar").gameObject.SetActive(true);
         transform.Find("Mike Highlight").gameObject.SetActive(!transform.Find("Mike Highlight").gameObject.activeInHierarchy);
         transform.Find("Bar").GetComponent<RawImage>().texture = allBars[Random.Range(0, 9)];
         Invoke("mikeTalking", Random.Range(0.1f, 0.5f));
+    }
+
+    public void textType()
+    {
+        if (currentPos < allLines[currentString].Length)
+        {
+            transform.Find("Text Line").GetComponent<Text>().text += "" + (allLines[currentString])[currentPos];
+            transform.Find("Text Line 2").GetComponent<Text>().text += "" + (allLines[currentString])[currentPos];
+            currentPos++;
+            Invoke("textType", 0.1f);
+        }
+        else
+        {
+            CancelInvoke("mikeTalking");
+            transform.Find("Mike Highlight").gameObject.SetActive(false);
+            transform.Find("Bar").gameObject.SetActive(false);
+        }
     }
 }
