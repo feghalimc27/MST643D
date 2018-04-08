aa7fce79d1da62ca14f282a6edbd8df537127ca0
mibikin <mibikin@gmail.com>
2018-04-07 18:29:39 -0600

    Limited movement to mov variable of unit
    yes

--- Assets/All Scenes/5. Flaming Symbol/Scripts/SelectionCursor.cs
+++ Assets/All Scenes/5. Flaming Symbol/Scripts/SelectionCursor.cs
@@ -8,7 +8,18 @@ public class SelectionCursor : MonoBehaviour {
 
 	private float speed = 0.5f;
 	private int coolCounter = 0;
+
 	private GameObject player = null;
+
+	private Vector3 startPos;
+	private float endPosPlusX;
+	private float endPosMinusX;
+	private float endPosPlusY;
+	private float endPosMinusY;
+
+	private int movCountX = 0;
+	private int movCountY = 0;
+
 	private bool unitSelected = false;
 
 	// Use this for initialization
@@ -40,26 +51,79 @@ public class SelectionCursor : MonoBehaviour {
 	void MoveCursor() {
 		Vector3 move = new Vector3(0, 0, 0);
 
-		if (Input.GetAxis("Horizontal") > 0.2 && coolCounter == 0) {
-			move.x += speed;
+		if (unitSelected) {
+			if ((transform.position + move).x < endPosPlusX && Input.GetAxis("Horizontal") > 0.2 && coolCounter == 0) {
+				if (Mathf.Abs(movCountX) + Mathf.Abs(movCountY) != player.gameObject.GetComponent<FEFriendlyUnit>().mov) {
+					move.x += speed;
+					movCountX += 1;
+				}
+				else if (movCountX < 0) {
+					move.x += speed;
+					movCountX += 1;
+				}
 
-			coolCounter = movementCooldown;
-		}
-		else if (Input.GetAxis("Horizontal") < -0.2 && coolCounter == 0) {
-			move.x -= speed;
+				coolCounter = movementCooldown;
+			}
+			else if ((transform.position - move).x > endPosMinusX && Input.GetAxis("Horizontal") < -0.2 && coolCounter == 0) {
+				if (Mathf.Abs(movCountX) + Mathf.Abs(movCountY) != player.gameObject.GetComponent<FEFriendlyUnit>().mov) {
+					move.x -= speed;
+					movCountX -= 1;
+				}
+				else if (movCountX > 0) {
+					move.x -= speed;
+					movCountX -= 1;
+				}
 
-			coolCounter = movementCooldown;
-		}
-		else if (Input.GetAxis("Vertical") > 0.2 && coolCounter == 0) {
-			move.y += speed;
+				coolCounter = movementCooldown;
+			}
+			else if ((transform.position + move).y < endPosPlusY && Input.GetAxis("Vertical") > 0.2 && coolCounter == 0) {
+				if (Mathf.Abs(movCountX) + Mathf.Abs(movCountY) != player.gameObject.GetComponent<FEFriendlyUnit>().mov) {
+					move.y += speed;
+					movCountY += 1;
+				}
+				else if (movCountY < 0) {
+					move.y += speed;
+					movCountY += 1;
+				}
 
-			coolCounter = movementCooldown;
-		}
-		else if (Input.GetAxis("Vertical") < -0.2 && coolCounter == 0) {
-			move.y -= speed;
+				coolCounter = movementCooldown;
+			}
+			else if ((transform.position - move).y > endPosMinusY && Input.GetAxis("Vertical") < -0.2 && coolCounter == 0) {
+				if (Mathf.Abs(movCountX) + Mathf.Abs(movCountY) != player.gameObject.GetComponent<FEFriendlyUnit>().mov) {
+					move.y -= speed;
+					movCountY -= 1;
+				}
+				else if (movCountY > 0) {
+					move.y -= speed;
+					movCountY -= 1;
+				}
 
-			coolCounter = movementCooldown;
+				coolCounter = movementCooldown;
+			}
 		}
+		else {
+			if (Input.GetAxis("Horizontal") > 0.2 && coolCounter == 0) {
+				move.x += speed;
+
+				coolCounter = movementCooldown;
+			}
+			else if (Input.GetAxis("Horizontal") < -0.2 && coolCounter == 0) {
+				move.x -= speed;
+
+				coolCounter = movementCooldown;
+			}
+			else if (Input.GetAxis("Vertical") > 0.2 && coolCounter == 0) {
+				move.y += speed;
+
+				coolCounter = movementCooldown;
+			}
+			else if (Input.GetAxis("Vertical") < -0.2 && coolCounter == 0) {
+				move.y -= speed;
+
+				coolCounter = movementCooldown;
+			}
+		}
+		
 
 		transform.position += move;
 		if (unitSelected) {
@@ -71,9 +135,20 @@ public class SelectionCursor : MonoBehaviour {
 		if (player != null) {
 			if (Input.GetButtonDown("Jump") && !unitSelected) {
 				unitSelected = true;
+
+				startPos = player.transform.position;
+				float movRange = player.GetComponent<FEFriendlyUnit>().mov * speed;
+
+				endPosPlusX = startPos.x + movRange;
+				endPosMinusX = startPos.x - movRange;
+				endPosPlusY = startPos.y + movRange;
+				endPosMinusY = startPos.y - movRange;
 			}
 			else if (Input.GetButtonDown("Jump") && unitSelected) {
 				unitSelected = false;
+
+				movCountX = 0;
+				movCountY = 0;
 			}
 		}
 	}
