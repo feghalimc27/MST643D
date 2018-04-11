3e2458067f8db4407bb5a4e336491288abda57d0
mibikin <mibikin@gmail.com>
2018-04-11 13:53:23 -0600

    Started enemy turn handling

---
+++ Assets/All Scenes/5. Flaming Symbol/Scripts/EnemyTurnManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTurnManager : MonoBehaviour {
+
+	private GameObject[] units;
+
+	private int currentUnit;
+
+	private bool turnOver = true;
+
+	// Use this for initialization
+	void Start () {
+		units = GetComponentsInChildren<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
