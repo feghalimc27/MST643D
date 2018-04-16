db18b2c55c16a03de0bb56fbe7a98019c5b46033
Uriah Moeller <uriah.moeller@ucdenver.edu>
2018-04-16 13:52:32 -0600

    WIP Super Seoul Ball Shit

--- Assets/All Scenes/2. Super Seoul Ball 3D/Scripts/BallMovement.cs
+++ Assets/All Scenes/2. Super Seoul Ball 3D/Scripts/BallMovement.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+//[RequireComponent(typeof(Rigidbody))]
 public class BallMovement : MonoBehaviour {
 
     public float acceleration;
+    public float torque;
     public float friction;
 
 	// Use this for initialization
@@ -18,12 +19,26 @@ public class BallMovement : MonoBehaviour {
 
     }
 
+
+    void OnTriggerEnter (Collider collision)
+    {
+        if (collision.gameObject.name == "End")
+        {
+            DontDestroyOnLoad(new GameObject("levelCompleted"));
+        }
+    }
+
     void FixedUpdate() {
         AccelerateBall();
     }
 
     void AccelerateBall() {
-        GetComponent<Rigidbody>().AddForce(acceleration * Input.GetAxis("Horizontal"), 0, acceleration * Input.GetAxis("Vertical"));
+        //transform.GetChild(0).position = transform.position;
+        //transform.rotation = transform.GetChild(0).rotation;
+        transform.GetComponent<Rigidbody>().AddForce(transform.GetChild(0).forward.x * acceleration * Input.GetAxis("Horizontal"), 0, transform.GetChild(0).forward.z * acceleration * Input.GetAxis("Vertical"));
+        transform.GetComponent<Rigidbody>().AddTorque(transform.GetChild(0).up * torque * Input.GetAxis("Horizontal"));
+        transform.GetChild(0).GetComponent<Rigidbody>().AddForce(transform.GetChild(0).forward.x * acceleration * Input.GetAxis("Horizontal"), 0, transform.GetChild(0).forward.z * acceleration * Input.GetAxis("Vertical"));
+        transform.GetChild(0).GetComponent<Rigidbody>().AddTorque(transform.GetChild(0).up * torque * Input.GetAxis("Horizontal"));
     }
 
     void ApplyFriction() {
