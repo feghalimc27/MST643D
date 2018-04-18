8145a76847a9ce7983b6b80fe61f392de109c001
Fernando <fernando.guardado-perez@ucdenver.edu>
2018-04-18 16:28:23 -0600

    SSS
    Mike can jump and shit

---
+++ Assets/All Scenes/6. Super Seoul Sisters/Scripts/BubberDucky.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class BubberDucky : MonoBehaviour {
+
+	private GameObject mikeMan;
+	public float enemySpeed = 1.7f;
+    public int xMoveDirection;
+	public int yMoveDirection = 1;
+	public float mikeRaycast = .2f;
+	public Rigidbody projectile;
+
+    void Start()
+    {
+        InvokeRepeating("JumpMikeJump", 1.0f, 1.3f);
+    }
+
+    void JumpMikeJump()
+    {
+       	gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 1000);
+		gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.right * 3000);
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(1, 0) * enemySpeed;
+	}
+
+	void YDirection()
+	{
+		if(yMoveDirection > 0)
+        {
+            yMoveDirection = -yMoveDirection;
+        }
+
+        else
+        {
+            yMoveDirection = +yMoveDirection;
+        }
+	}
+}
+
---
+++ Assets/All Scenes/6. Super Seoul Sisters/Scripts/Mike_Camera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mike_Camera : MonoBehaviour {
+
+    private GameObject player;
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
+
+    void Update(){
+        if((player.transform.position.x > 38.85f && player.transform.position.x < 40.4f) && player.transform.position.y < -3.614f)
+        {
+            yMin = -3.905f;
+        }
+        else if(player.transform.position.y > -3.0f)
+        {
+            yMin = -1.47f;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        float x = Mathf.Clamp(player.transform.position.x, xMin, xMax);
+        float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
+        gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
+    }
+}
