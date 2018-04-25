5f2e7b021b5b01775961ede65fe0d6b32a324aaa
mibikin <mibikin@gmail.com>
2018-04-25 12:51:55 -0600

    Begun Master Foot

---
+++ Assets/All Scenes/10. Master Foot/Scripts/Platform Fighter Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFighterMovement : MonoBehaviour {
+
+	[SerializeField]
+	private float acceleration, airAccel, friction, jumpStrength, fallSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
