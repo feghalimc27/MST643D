29c158722f7426fde572222e42b9b7d3ea60370c
mo-hossain <10749376+mo-hossain@users.noreply.github.com>
2018-04-26 01:05:53 -0600

    some updates before i sleep
    Most of this stuff is tentative cause im figuring out what way im doing this. i may remove it later.

---
+++ Assets/All Scenes/8. Seeing Books/Scripts/DialogueParser.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+public class DialogueParser : MonoBehaviour {
+
+    struct DialogueLine
+    {
+        public string name;
+        public string content;
+        public string position;
+        public string[] options;
+
+        public DialogueLine(string Name, string Content, string Position)
+        {
+            name = Name;
+            content = Content;
+            position = Position;
+            options = new string[0];
+        }
+    }
+
+    List<DialogueLine> lines;
+
+    void Start()
+    {
+        string file = "Assets/All Scenes/8. Seeing Books/Dialogue";
+        string sceneNum = EditorSceneManager.GetActiveScene().name;
+        sceneNum = Regex.Replace(sceneNum, "[^0-9]", "");
+        file += sceneNum;
+        file += ".txt.";
+
+        lines = new List<DialogueLine>();
+
+        LoadDialogue(file);
+    }
+
+}
