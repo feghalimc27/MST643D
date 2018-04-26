3683151321266a17d55f229be7119f7b643de193
FernandoGuardado <35590251+FernandoGuardado@users.noreply.github.com>
2018-04-26 04:28:50 -0600

    SSS
    Added audio to rings, jumping, landing (disabled atm because I hate it), dying, Mike & his duck.
    Fixed other stuff that I can't think of right now.

--- Assets/All Scenes/6. Super Seoul Sisters/Scripts/BubberDucky.cs
+++ Assets/All Scenes/6. Super Seoul Sisters/Scripts/BubberDucky.cs
@@ -7,9 +7,21 @@ using UnityEngine.SceneManagement;
 public class BubberDucky : MonoBehaviour {
     public GameObject merry;
 
+    public AudioClip soundToPlay;
+    public AudioSource landSound;
+    public AudioClip playDeath;
+    public AudioSource deathSound;
+
+
     void Start()
     {
+        merry.GetComponent<Character_Move>().enabled = false;
+
+        landSound = GetComponent<AudioSource>();    // assign AudioSource
+        deathSound = GetComponent<AudioSource>();    // assign AudioSource
+
         Physics2D.IgnoreLayerCollision(12, 13);
+        StartCoroutine(EnableMovementAfterDelay(1.5f));
         InvokeRepeating("JumpMikeJump", 2.0f, 1.0f);
     }
 
@@ -23,6 +35,7 @@ public class BubberDucky : MonoBehaviour {
     {
         if(collision.name == "Merry")
         {
+            deathSound.PlayOneShot(playDeath); // play sound
             merry.GetComponent<Rigidbody2D>().gravityScale = 5;
             merry.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 200);
             merry.GetComponent<Rigidbody2D>().gravityScale = 2;
@@ -30,12 +43,25 @@ public class BubberDucky : MonoBehaviour {
             merry.GetComponent<Rigidbody2D>().transform.eulerAngles = new Vector3(0, 0, 180);
             merry.GetComponent<BoxCollider2D>().enabled = false;
             merry.GetComponent<Rigidbody2D>().gravityScale = 5;
+            merry.GetComponent<Character_Move>().enabled = false;
+            Camera.main.GetComponent<Camera_System>().enabled = false;
 
             if (merry.transform.position.y < -6.38)
             {
                 SceneManager.LoadScene("Super Seoul Sisters");
             }
         }
+        else if(collision.name != "Merry")
+        {
+            landSound.PlayOneShot(soundToPlay); // play sound
+        }
     }
+
+    IEnumerator EnableMovementAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        merry.GetComponent<Character_Move>().enabled = true;
+    }
+
 }
 
