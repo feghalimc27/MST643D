bcf29356f0e4aaa547ae45416fd56d6ace35a9d7
FernandoGuardado <35590251+FernandoGuardado@users.noreply.github.com>
2018-04-26 00:39:08 -0600

    SSS
    Added coins & coin sounds

---
+++ Assets/All Scenes/6. Super Seoul Sisters/Scripts/Rings.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rings : MonoBehaviour
+{
+
+    private float speed = 80;
+    public AudioClip soundToPlay;
+    public AudioSource ringSound;
+
+    // Use this for initialization
+    void Start()
+    {
+        ringSound = GetComponent<AudioSource>();    // assign AudioSource
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);   // rotate coin
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Merry")
+        {
+            ringSound.PlayOneShot(soundToPlay); // play sound
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;  // disable sprite, can't destroy right away because it will not play sound
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;   // disable collider so it only plays once
+
+            Destroy(gameObject, 1f);    // destroy after one second
+        }
+    }
+}
