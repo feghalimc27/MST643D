20a9428b9c86a866b1c2be2d1756d35a98b3c649
Sguishh <36168912+Sguishh@users.noreply.github.com>
2018-04-28 21:09:29 -0600

    Signifigant Progress, more tonight
    Made assets for the enemies, projectiles, and Merry.  Gonna finish the level design tonight and make the projectiles to stuff when they hit things.

--- Assets/All Scenes/7. Basement Descent/BasementDescentAssets/playerControl.cs
+++ Assets/All Scenes/7. Basement Descent/BasementDescentAssets/playerControl.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class playerControl : MonoBehaviour {
 
     private float xMove, yMove, xFacing, yFacing;
+    private float shotCooldown = 0;
     private Vector2 moveVector;
     public float speed = 3;
+    public float projDelay = 0.5f;
+    public Transform bulletObj;
 
 	// Use this for initialization
 	void Start () {
@@ -26,22 +29,33 @@ public class playerControl : MonoBehaviour {
         yFacing = -Input.GetAxis("VerticalR");
 
         if (yFacing > 0.3 || yFacing < -0.3 || xFacing > 0.3 || xFacing < -0.3)
+        {
             transform.rotation = Quaternion.Euler(0, 0, (Mathf.Atan2(xFacing, yFacing) * (180 / 3.14f)));
+            
+        }
     }
 
     void PlayerAttack()
     {
-
+        Instantiate(bulletObj, transform.position, bulletObj.rotation);
     }
 	
 	// Update is called once per frame
 	void Update () {
 
         PlayerMovement();
-        
-        if(Input.GetKey("Fire2"))
+
+        if (Input.GetAxis("backTriggers") > 0.9 && shotCooldown == 0)
         {
-           
+            shotCooldown = 1;
+            PlayerAttack();
+            StartCoroutine(shotDelayReset());
         }
+    }
+
+    IEnumerator shotDelayReset()
+    {
+        yield return new WaitForSeconds(projDelay);
+        shotCooldown = 0;
     }
 }
