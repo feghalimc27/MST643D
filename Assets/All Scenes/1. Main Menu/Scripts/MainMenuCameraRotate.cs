aa8b76bd7dfb6d27595ea2fe4da1e74744ed778c
mibikin <mibikin@gmail.com>
2018-04-29 23:11:17 -0600

    Added cool menu spin animation

---
+++ Assets/All Scenes/1. Main Menu/Scripts/MainMenuCameraRotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCameraRotate : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Submit")) {
+            transform.Rotate(Vector3.up * speed * 3 * Time.deltaTime);
+        }
+        else {
+            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+        }
+    }
+}
