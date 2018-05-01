f2e461a97a2bc30b43e2009bd6b4a88b597cce58
mibikin <mibikin@gmail.com>
2018-04-30 23:19:45 -0600

    Go fast for density; also redid the movement
    You might be too fast but holding left bumper while in the air gives you a burst forward in the direction being held. Probably need to slow it down. The standard walking was also redone

--- Assets/All Scenes/3. Density/Scripts/FPSMovement.cs
+++ Assets/All Scenes/3. Density/Scripts/FPSMovement.cs
@@ -8,28 +8,41 @@ public class FPSMovement: MonoBehaviour {
     public float speed = 10.0f;
     public float jumpStrength = 25.0f;
 	public int fireRate = 1200;
+    public float liftForce = 700;
+    public float liftCooldown = 15.0f;
+    public float friction = 0.34f;
 
     private bool onGround = false;
+    private bool lifting = false;
+    private float _liftCooldown;
+    [SerializeField]
+    private int initialLiftMultiplier = 3;
+
+    private Vector3 boostRate = new Vector3(0, 0, 0);
 
 	// Use this for initialization
 	void Start () {
         Cursor.lockState = CursorLockMode.Locked;
+        _liftCooldown = liftCooldown;
+        liftCooldown *= 3;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        Debug.Log("Forward Velcity: " + GetComponent<Rigidbody>().velocity.z);
     }
 
     // Called at a consistent point per frame
     void FixedUpdate() {
         MovePlayer();
         Jump();
+        Lift();
     }
 
     void OnCollisionEnter(Collision col) {
         if (col.gameObject.layer == 8) {
             onGround = true;
+            lifting = false;
         }
     }
 
@@ -37,6 +50,7 @@ public class FPSMovement: MonoBehaviour {
         // Layer 8 = Ground
         if (col.gameObject.layer == 8) {
             onGround = true;
+            lifting = false;
         }
     }
 
@@ -57,8 +71,10 @@ public class FPSMovement: MonoBehaviour {
         strafePos *= Time.deltaTime;
         forwardPos *= Time.deltaTime;
 
+        Vector3 movePosition = new Vector3(strafePos, 0, forwardPos);
+
         // Translate by strength
-        transform.Translate(strafePos, 0, forwardPos);
+        GetComponent<Rigidbody>().MovePosition((transform.position + transform.TransformDirection(movePosition)));
 
         // Allow cursor to be freed on Esc press
         if (Input.GetKeyDown(KeyCode.Escape)) {
@@ -69,6 +85,61 @@ public class FPSMovement: MonoBehaviour {
     void Jump() {
         if (onGround && Input.GetButtonDown("Jump")) {
             GetComponent<Rigidbody>().AddForce(new Vector3(0, jumpStrength, 0));
+        }
+        if (!onGround && GetComponent<Rigidbody>().velocity.y < 0) {
+            GetComponent<Rigidbody>().AddForce(Physics.gravity);
+        }
+    }
+
+    void Lift() {
+        if (!onGround) {
+            if (Input.GetButton("Lift") && liftCooldown > 0) {
+                if (Input.GetButtonDown("Lift")) {
+                    if (liftCooldown <= 0) {
+                        liftCooldown = 0;
+                    }
+
+                    if (!lifting) {
+                        boostRate = new Vector3(1, 1, 1);
+                        lifting = true;
+                    }
+
+                    boostRate.z = 1;
+                    boostRate.y = 0;
+                    boostRate.x = 1;
+                    boostRate.z *= liftForce * initialLiftMultiplier * Input.GetAxis("Vertical");
+                    boostRate.x *= liftForce * initialLiftMultiplier * Input.GetAxis("Horizontal");
+                }
+                else {
+                    if (liftCooldown <= 0) {
+                        liftCooldown = 0;
+                    }
+
+                    if (!lifting) {
+                        boostRate = new Vector3(1, 1, 1);
+                        lifting = true;
+                    }
+                    boostRate.z = 1;
+                    boostRate.y = (-Physics.gravity.y) + jumpStrength / (jumpStrength / (1 + (liftCooldown / _liftCooldown)));
+                    boostRate.x = 1;
+                    boostRate.z *= liftForce * initialLiftMultiplier * liftCooldown / _liftCooldown * (Input.GetAxis("Vertical"));
+                    boostRate.x *= liftForce * initialLiftMultiplier * liftCooldown / _liftCooldown * (Input.GetAxis("Horizontal"));
+                }
+                GetComponent<Rigidbody>().AddRelativeForce(boostRate);
+                liftCooldown -= 15f * Time.deltaTime;
+            }
+            else {
+
+            }
+        }
+
+        else if (onGround) {
+            if (liftCooldown < _liftCooldown * 3) {
+                liftCooldown += 10f * Time.deltaTime;
+            }
+            else if (liftCooldown > _liftCooldown * 3) {
+                liftCooldown = _liftCooldown * 3;
+            }
         }
     }
 }
